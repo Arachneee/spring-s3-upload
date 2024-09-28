b2205edc93bb29e2120a7353dfe4103df8a042f2
Arachne <66822642+Arachneee@users.noreply.github.com>
2024-09-28 13:21:00 +0900

    feat: init

---
+++ src/main/java/org/example/springs3upload/SpringS3UploadApplication.java
@@ -0,0 +1,13 @@
+package org.example.springs3upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringS3UploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringS3UploadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springs3upload/SpringS3UploadApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springs3upload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringS3UploadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
