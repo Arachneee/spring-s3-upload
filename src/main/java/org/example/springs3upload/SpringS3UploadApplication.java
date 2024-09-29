f1efca4d49228e6482624bcdcd6d2984a7da34f5
Arachne <66822642+Arachneee@users.noreply.github.com>
2024-09-30 01:25:05 +0900

    feat: 톰캣 설정

--- src/main/java/org/example/springs3upload/SpringS3UploadApplication.java
+++ src/main/java/org/example/springs3upload/SpringS3UploadApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableAsync
 public class SpringS3UploadApplication {
 
     public static void main(String[] args) {
