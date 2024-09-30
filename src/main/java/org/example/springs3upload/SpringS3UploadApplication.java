187e6bd81976640d6e3c59269f17e920ae1e3505
Arachne <66822642+Arachneee@users.noreply.github.com>
2024-09-30 14:00:45 +0900

    feat: 톰캣 설정

--- src/main/java/org/example/springs3upload/SpringS3UploadApplication.java
+++ src/main/java/org/example/springs3upload/SpringS3UploadApplication.java
@@ -2,13 +2,14 @@ package org.example.springs3upload;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 //@EnableAsync
 public class SpringS3UploadApplication {
 
     public static void main(String[] args) {
+        int maxFD = (int) Runtime.getRuntime().maxMemory();
+        System.out.println("########## Current file descriptor limit: " + maxFD);
         SpringApplication.run(SpringS3UploadApplication.class, args);
     }
 
