964dfe5e0b2fcf57e6e19ecab729438f2c7767ef
Arachne <66822642+Arachneee@users.noreply.github.com>
2024-10-07 21:16:00 +0900

    feat: apach config

--- src/main/java/org/example/springs3upload/s3/async/ImageNameAppResponse.java
+++
@@ -1,8 +0,0 @@
-package org.example.springs3upload.s3.async;
-
-public record ImageNameAppResponse(String name) {
-
-    public EventImage toEventImage(Event event) {
-        return new EventImage(event, name);
-    }
-}
